d70e367daf22f98f36097f09e5a7fbb0950d38d9
Jimit <jp47@njit.edu>
2023-10-03 15:27:21 -0400

    milestone3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './components/HomePage';
 import Customers from './components/Customers';
 import Movies from './components/Movies';
+import NotFound from './components/notfound';
 import { BrowserRouter, Routes, Route,} from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<HomePage />}/>
           <Route path="/customers" element={<Customers />}/>
           <Route path="/Movies" element={<Movies />}/>
+          <Route path="/*" element={<NotFound />}/>
         </Routes>
       </BrowserRouter>
     </div>
